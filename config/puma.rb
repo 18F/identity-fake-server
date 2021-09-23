9be1203c2ffbb665557be3aa46813265d435bb89
Justin Grevich <justin@grevi.ch>
2021-09-22 17:09:14 -0700

    Log Puma stats (#15)

--- config/puma.rb
+++ config/puma.rb
@@ -1,2 +1,14 @@
 threads ENV['PUMA_MIN_THREADS'].to_i, ENV['PUMA_MAX_THREADS'].to_i
 workers ENV['PUMA_NUM_WORKERS'].to_i
+
+# Invoke Puma.stats from the main process
+if ENV['LOG_PUMA_STATS']
+  before_fork do
+    Thread.new do
+      loop do
+        puts Puma.stats
+        sleep 30
+      end
+    end
+  end
+end
